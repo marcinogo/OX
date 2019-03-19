5355e5b3341c18e61a49d7912c55500a2ea32c78
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-03-19 22:28:56 +0100

    Create test print of empty Board

--- src/main/java/ogo/marcin/ox/Main.java
+++ src/main/java/ogo/marcin/ox/Main.java
@@ -1,10 +1,17 @@
 package ogo.marcin.ox;
 
+import ogo.marcin.ox.board.Board;
+import ogo.marcin.ox.board.BoardFactory;
+import ogo.marcin.ox.board.BoardFactoryImpl;
+import ogo.marcin.ox.board.Sign;
+
 /**
  * @author Marcin Ogorzalek
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        BoardFactory boardFactory = new BoardFactoryImpl();
+        Board board = boardFactory.createBoard(3, 3, Sign.DEFAULT);
+        System.out.println(board);
     }
 }
