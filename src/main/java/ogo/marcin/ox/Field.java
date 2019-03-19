f7e13382f99234fb4813091391be09e3734a9fa0
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-19 08:12:39 +0100

    Implement method equals()

--- src/main/java/ogo/marcin/ox/Field.java
+++ src/main/java/ogo/marcin/ox/Field.java
@@ -1,5 +1,7 @@
 package ogo.marcin.ox;
 
+import java.util.Objects;
+
 /**
  * @author Marcin Ogorzalek
  */
@@ -9,5 +11,13 @@ public class Field {
 
     public Field(Sign sign) {
         this.sign = sign;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Field field = (Field) o;
+        return sign == field.sign;
     }
 }
--- src/test/java/ogo/marcin/ox/FieldTest.java
+++ src/test/java/ogo/marcin/ox/FieldTest.java
@@ -12,4 +12,10 @@ public class FieldTest {
         Field field = new Field(Sign.DEFAULT);
         assert field.sign.equals(Sign.DEFAULT) : "Field have different sign than DEFAULT";
     }
+
+    public void testIfFieldsAreEqual() {
+        Field field1 = new Field(Sign.DEFAULT);
+        Field field2 = new Field(Sign.DEFAULT);
+        assert field1.equals(field2) : "Fields should be equal";
+    }
 }
