418280958ab44ad103b2484f1e1ab1caea7d88b2
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-19 15:54:07 +0100

    Create Main class with main() method

---
+++ src/main/java/ogo/marcin/ox/Main.java
@@ -0,0 +1,10 @@
+package ogo.marcin.ox;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
