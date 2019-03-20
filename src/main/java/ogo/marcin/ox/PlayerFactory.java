13f18c87063c5ed53406f8c75d49236b91a74176
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-20 10:38:07 +0100

    Create PlayerFactory

---
+++ src/main/java/ogo/marcin/ox/PlayerFactory.java
@@ -0,0 +1,16 @@
+package ogo.marcin.ox;
+
+import ogo.marcin.ox.board.Sign;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+public class PlayerFactory {
+    Player createPlayer(String name, Sign sign) {
+        return new Player(name, sign);
+    }
+
+    Player createPlayer(String name, Sign sign, Integer points) {
+        return new Player(name, sign, points);
+    }
+}
---
+++ src/test/java/ogo/marcin/ox/PlayerFactoryTest.java
@@ -0,0 +1,63 @@
+package ogo.marcin.ox;
+
+import ogo.marcin.ox.board.Sign;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+public class PlayerFactoryTest {
+
+    @DataProvider
+    public static Object[][] createPlayerWithGivenPoints(){
+        return new Object[][] {
+                {"Player 1", Sign.X, 0},
+                {"Player 1", Sign.O, 0},
+                {"Player 2", Sign.X, 10},
+                {"Player 2", Sign.O, 10},
+                {"Tomek", Sign.X, 0},
+                {"Tomek", Sign.O, 0},
+                {"Tomek", Sign.X, 20},
+                {"Tomek", Sign.O, 20},
+                {"Tomek", Sign.X, 3},
+                {"Tomek", Sign.O, 3},
+        };
+    }
+
+    @Test(dataProvider = "createPlayerWithGivenPoints")
+    public void testCreatePlayerWithGivenPoints(String name, Sign sign, Integer points) {
+        PlayerFactory playerFactory = new PlayerFactory();
+        Player player = playerFactory.createPlayer(name, sign, points);
+        Player expected = new Player(name, sign, points);
+
+        assert player != null : "Player should not be null";
+        assert player.equals(expected) : "Players should be equals";
+    }
+
+    @DataProvider
+    public static Object[][] createPlayer(){
+        return new Object[][] {
+                {"Player 1", Sign.X},
+                {"Player 1", Sign.O},
+                {"Player 2", Sign.X},
+                {"Player 2", Sign.O},
+                {"Tomek", Sign.X},
+                {"Tomek", Sign.O},
+                {"Tomek", Sign.X},
+                {"Tomek", Sign.O},
+                {"Tomek", Sign.X},
+                {"Tomek", Sign.O},
+        };
+    }
+
+    @Test(dataProvider = "createPlayer")
+    public void testCreatePlayer(String name, Sign sign) {
+        PlayerFactory playerFactory = new PlayerFactory();
+        Player player = playerFactory.createPlayer(name, sign);
+        Player expected = new Player(name, sign);
+
+        assert player != null : "Player should not be null";
+        assert player.equals(expected) : "Players should be equals";
+    }
+}
