e0a935fb53be255109201da1246248bd40fc7b07
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-20 14:22:16 +0100

    Fix wrong information for user at failed assert

--- src/test/java/ogo/marcin/ox/MatchTest.java
+++ src/test/java/ogo/marcin/ox/MatchTest.java
@@ -14,13 +14,13 @@ public class MatchTest {
     public void testIfMachHaveWinningCondition() {
         BoardAPI boardAPI = new BoardAPIImpl();
         Match match = new Match(3, boardAPI.createBoard(3, 3, Sign.DEFAULT));
-        assert match.winCondition == 3 : "Winn condition should be equal 3";
+        assert match.winCondition == 3 : "Win condition should be equal 3";
     }
-    
+
     public void testIfMachHaveBoard() {
         BoardAPI boardAPI = new BoardAPIImpl();
         Board board = boardAPI.createBoard(3, 3, Sign.DEFAULT);
         Match match = new Match(3, board);
-        assert match.board ==  board: "Winn condition should be equal 3";
+        assert match.board ==  board: "Match should have board";
     }
 }
