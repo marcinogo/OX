859953395abbdc252b4b38422bb544e280209b4f
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-20 09:30:45 +0100

    Implement sign validation for null case

--- src/main/java/ogo/marcin/ox/Player.java
+++ src/main/java/ogo/marcin/ox/Player.java
@@ -11,11 +11,17 @@ class Player {
 
     Player(String name, Sign playerSign) {
         if(!validateName(name)) throw new IllegalArgumentException("Player must have name");
+        if(!validateSign(playerSign)) throw new IllegalArgumentException("Player must have not default sign");
+
         this.name = name;
         this.playerSign = playerSign;
     }
 
     private boolean validateName(String name) {
         return name != null && !name.equals("");
+    }
+
+    private boolean validateSign(Sign sign) {
+        return sign != null;
     }
 }
--- src/test/java/ogo/marcin/ox/PlayerTest.java
+++ src/test/java/ogo/marcin/ox/PlayerTest.java
@@ -17,12 +17,18 @@ public class PlayerTest {
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testIfCreatePlayerWithoutNameThrowsException() {
         Player player = new Player(null, Sign.X);
-        assert player.name.equals("Player 1") : "Player should do not throw NPE";
+        assert player.name != null : "Player should do not throw NPE";
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testIfCreatePlayerWithEmptyThrowsException() {
+    public void testIfCreatePlayerWithEmptyNameThrowsException() {
         Player player = new Player("", Sign.X);
-        assert player.name.equals("Player 1") : "Player should not be empty";
+        assert !player.name.equals("") : "Player should not be empty";
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testIfCreatePlayerWithoutSignThrowsException() {
+        Player player = new Player("Player 1", null);
+        assert player.playerSign != null : "Player should have sign";
     }
 }
