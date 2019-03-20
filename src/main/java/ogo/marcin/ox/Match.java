886c16b96c36013e6484d1c883e449e807ae399d
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-20 15:15:06 +0100

    Add checkFreeSpaceOnBoard() method

--- src/main/java/ogo/marcin/ox/Match.java
+++ src/main/java/ogo/marcin/ox/Match.java
@@ -1,16 +1,24 @@
 package ogo.marcin.ox;
 
 import ogo.marcin.ox.board.Board;
+import ogo.marcin.ox.board.BoardAPI;
+import ogo.marcin.ox.board.Sign;
 
 /**
  * @author Marcin Ogorzalek
  */
 class Match {
     Integer winCondition;
+    BoardAPI boardAPI;
     Board board;
 
-    Match(Integer winCondition, Board board) {
+    Match(Integer winCondition, BoardAPI boardAPI, Board board) {
         this.winCondition = winCondition;
+        this.boardAPI = boardAPI;
         this.board = board;
+    }
+
+    Boolean checkFreeSpaceOnBoard(Sign deafualSign) {
+        return boardAPI.isFreeSpaceOnBoard(board, deafualSign);
     }
 }
--- src/test/java/ogo/marcin/ox/MatchTest.java
+++ src/test/java/ogo/marcin/ox/MatchTest.java
@@ -13,14 +13,22 @@ import org.testng.annotations.Test;
 public class MatchTest {
     public void testIfMachHaveWinningCondition() {
         BoardAPI boardAPI = new BoardAPIImpl();
-        Match match = new Match(3, boardAPI.createBoard(3, 3, Sign.DEFAULT));
+        Match match = new Match(3, boardAPI, boardAPI.createBoard(3, 3, Sign.DEFAULT));
         assert match.winCondition == 3 : "Win condition should be equal 3";
     }
 
     public void testIfMachHaveBoard() {
         BoardAPI boardAPI = new BoardAPIImpl();
         Board board = boardAPI.createBoard(3, 3, Sign.DEFAULT);
-        Match match = new Match(3, board);
+        Match match = new Match(3, boardAPI, board);
         assert match.board ==  board: "Match should have board";
+    }
+
+    public void testCheckIfDefaultFieldsOnBoard() {
+        BoardAPI boardAPI = new BoardAPIImpl();
+        Board board = boardAPI.createBoard(3, 3, Sign.DEFAULT);
+        Match match = new Match(3, boardAPI, board);
+        Boolean freeSpace = match.checkFreeSpaceOnBoard(Sign.DEFAULT);
+        assert freeSpace == true : "There should be information that there is free space on board";
     }
 }
