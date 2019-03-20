28042b33131ea118ee1d014734b7db7b7acf9a1c
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-20 08:54:38 +0100

    Test testSetField()

---
+++ src/test/java/ogo/marcin/ox/board/BoardAPIImplTest.java
@@ -0,0 +1,66 @@
+package ogo.marcin.ox.board;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * @author Marcin Ogorzalek
+ */
+@Test
+public class BoardAPIImplTest {
+    @DataProvider
+    public static Object[][] createBoard(){
+        return new Object[][] {
+                {3, 3, Sign.DEFAULT},
+                {3, 5, Sign.O},
+                {5, 5, Sign.X},
+                {9, 7, Sign.X},
+                {4, 4, Sign.DEFAULT},
+                {40, 36, Sign.X},
+                {40, 40, Sign.O},
+                {39, 3, Sign.DEFAULT},
+                {3, 39, Sign.DEFAULT},
+                {3, 40, Sign.X},
+                {40, 3, Sign.O},
+                {20, 20, Sign.DEFAULT},
+        };
+    }
+
+    @Test(dataProvider = "createBoard")
+    public void testCreateBoard(int width, int height, Sign sign) {
+        BoardAPI boardAPI = new BoardAPIImpl();
+        Board board = boardAPI.createBoard(width, height, sign);
+        Board expectedBoard = new Board(width, height).setBoardMatrixCells(sign);
+
+        assert board != null : "Board should not be null";
+        assert board.equals(expectedBoard) : "Board should be equals";
+    }
+
+    @DataProvider
+    public static Object[][] setFieldValue(){
+        return new Object[][] {
+                {3, 3, 1, 1 , Sign.DEFAULT, Sign.O},
+                {3, 3, 1, 1 , Sign.DEFAULT, Sign.X},
+                {3, 5, 2, 4, Sign.O, Sign.X},
+                {3, 5, 2, 4, Sign.O, Sign.DEFAULT},
+                {4, 4, 2, 3, Sign.DEFAULT, Sign.X},
+                {4, 4, 2, 3, Sign.DEFAULT, Sign.O},
+                {40, 36, 35, 25, Sign.X, Sign.O},
+                {40, 36, 35, 25, Sign.X, Sign.DEFAULT},
+                {40, 40, 39, 39, Sign.O, Sign.X},
+                {40, 40, 39, 39, Sign.O, Sign.DEFAULT},
+        };
+    }
+
+    @Test(dataProvider = "setFieldValue")
+    public void testSetField(int width, int height, int widthToUpdate,
+                             int heightToUpdate, Sign defaultSign, Sign newSign) {
+        BoardAPI boardAPI = new BoardAPIImpl();
+        Board defaultBoard = new Board(width, height).setBoardMatrixCells(defaultSign);
+        Board result = boardAPI.setField(defaultBoard, widthToUpdate, heightToUpdate, newSign);
+        assert !defaultBoard.equals(result) : "Boards should not be equals";
+        defaultBoard.matrix[heightToUpdate][widthToUpdate] = new Field(newSign);
+        assert defaultBoard.equals(result) : "Boards should be equals";
+    }
+
+}
