4229a8936b1e70bcb21def47bccff880ebd65e85
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-21 14:41:01 +0100

    Restart Match class

--- src/main/java/ogo/marcin/ox/Match.java
+++ src/main/java/ogo/marcin/ox/Match.java
@@ -1,22 +1,8 @@
 package ogo.marcin.ox;
 
-import ogo.marcin.ox.board.Board;
-import ogo.marcin.ox.board.BoardAPI;
-import ogo.marcin.ox.board.Sign;
-
 /**
  * @author Marcin Ogorzalek
  */
 class Match {
-    Integer winCondition;
-    BoardAPI boardAPI;
-    Board board;
-
-    Match(Integer winCondition, BoardAPI boardAPI, Board board) {
-        this.winCondition = winCondition;
-        this.boardAPI = boardAPI;
-        this.board = board;
-    }
-
 
 }
