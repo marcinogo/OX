6d615126322c568879f7f800318a77b66cb5dcb3
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-03-21 19:13:39 +0100

    Add field board

--- src/main/java/ogo/marcin/ox/Judge.java
+++ src/main/java/ogo/marcin/ox/Judge.java
@@ -12,26 +12,32 @@ class Judge {
     private BoardAPI boardAPI;
     private Sign defaultSign;
     private Integer winCondition;
+    private Board board;
 
-    public Judge(BoardAPI boardAPI, Sign defaultSign, Integer winCondition) {
+    Judge(BoardAPI boardAPI, Sign defaultSign, Integer winCondition, Board board) {
         this.boardAPI = boardAPI;
         this.defaultSign = defaultSign;
         this.winCondition = winCondition;
+        this.board = board;
     }
 
-    Boolean isFreeSpaceOnBoard(Board board) {
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    Boolean isFreeSpaceOnBoard() {
         return boardAPI.isFreeSpaceOnBoard(board, defaultSign);
     }
 
-    Boolean isPlayerActionWithinBoard(Board board, Coordinates coordinates) {
+    Boolean isPlayerActionWithinBoard(Coordinates coordinates) {
         return boardAPI.isCoordinatesWithinBoard(board, coordinates);
     }
 
-    Boolean isPlayerSignSetOnFreeSpace(Board board, Coordinates coordinates) {
+    Boolean isPlayerSignSetOnFreeSpace(Coordinates coordinates) {
         return boardAPI.isCoordinatesPointsToDefaultSign(board, defaultSign, coordinates);
     }
 
-    Boolean isPlayerWon(Board board, Sign playerSign, Coordinates coordinates) {
+    Boolean isPlayerWon(Sign playerSign, Coordinates coordinates) {
         return boardAPI.isSignNumberMeetRequirements(board, playerSign, coordinates, winCondition);
     }
 }
