2503724cc93d004e189a16bbb9955091a07b27eb
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-03-21 15:47:29 +0100

    Create test match loop for 1 player

--- src/main/java/ogo/marcin/ox/Main.java
+++ src/main/java/ogo/marcin/ox/Main.java
@@ -1,5 +1,14 @@
 package ogo.marcin.ox;
 
+import ogo.marcin.ox.board.BoardAPI;
+import ogo.marcin.ox.board.BoardAPIImpl;
+import ogo.marcin.ox.board.Sign;
+import ogo.marcin.ox.player.Player;
+import ogo.marcin.ox.player.PlayerAPI;
+import ogo.marcin.ox.player.PlayerAPIImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,8 +18,16 @@ public class Main {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             Input input = new Input(scanner);
-            System.out.println(input.getStringInput());
-            System.out.println(input.getIntegerInput());
+            BoardAPI boardAPI = new BoardAPIImpl();
+            Sign defaultSign = Sign.DEFAULT;
+            PlayerAPI playerAPI = new PlayerAPIImpl();
+            List<Player> players = new ArrayList<>();
+            players.add(playerAPI.createPlayer("player 1", Sign.X));
+            players.add(playerAPI.createPlayer("player 2", Sign.X));
+            Integer winCondition = 3;
+
+            Match match = new Match(boardAPI, input, players, defaultSign, winCondition, playerAPI);
+            match.play();
         }
     }
 }
--- src/main/java/ogo/marcin/ox/Match.java
+++ src/main/java/ogo/marcin/ox/Match.java
@@ -1,8 +1,61 @@
 package ogo.marcin.ox;
 
+import ogo.marcin.ox.board.Board;
+import ogo.marcin.ox.board.BoardAPI;
+import ogo.marcin.ox.board.Coordinates;
+import ogo.marcin.ox.board.Sign;
+import ogo.marcin.ox.player.Player;
+import ogo.marcin.ox.player.PlayerAPI;
+
+import java.util.List;
+
 /**
  * @author Marcin Ogorzalek
  */
 class Match {
+    private BoardAPI boardAPI;
+    private Input input;
+    private List<Player> players;
+    private Sign defaultSign;
+    private Integer winCondition;
+    private PlayerAPI playerAPI;
 
+    public Match(BoardAPI boardAPI, Input input, List<Player> players,
+                 Sign defaultSign, Integer winCondition, PlayerAPI playerAPI) {
+        this.boardAPI = boardAPI;
+        this.input = input;
+        this.players = players;
+        this.defaultSign = defaultSign;
+        this.winCondition = winCondition;
+        this.playerAPI = playerAPI;
+    }
+
+    void play() {
+        Coordinates coordinates = new Coordinates(3, 3);
+        Board board = boardAPI.createBoard(coordinates, defaultSign);
+        Judge judge = new Judge(boardAPI, defaultSign, winCondition);
+
+        System.out.println(board);
+        Boolean isWinner = Boolean.FALSE;
+        do {
+            System.out.println(players.get(0));
+            board = boardAPI.setField(board, getCoordinates(board), playerAPI.getPlayerSign(players.get(0)));
+            System.out.println(board);
+
+        } while (judge.isFreeSpaceOnBoard(board) && !isWinner);
+    }
+
+    private Coordinates getCoordinates(Board board) {
+        Coordinates coordinates = null;
+         do {
+             System.out.println("Enter x coordinate");
+             Integer x = input.getIntegerInput();
+             System.out.println("Enter y coordinate");
+             Integer y = input.getIntegerInput();
+//             TODO check and info if they are correct;
+             coordinates = new Coordinates(x, y);
+         } while (!boardAPI.isCoordinatesWithinBoard(board, coordinates) ||
+                 !boardAPI.isCoordinatesPointsToDefaultSign(board, defaultSign, coordinates));
+        return coordinates;
+    }
 }
