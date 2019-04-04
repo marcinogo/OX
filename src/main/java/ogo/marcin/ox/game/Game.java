c00d9ca5cf47d3598655220251a44ae9a5677326
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-04-04 18:03:49 +0200

    Remove unused field

--- src/main/java/ogo/marcin/ox/game/Game.java
+++ src/main/java/ogo/marcin/ox/game/Game.java
@@ -2,11 +2,11 @@ package ogo.marcin.ox.game;
 
 import ogo.marcin.ox.board.BoardAPI;
 import ogo.marcin.ox.io.Input;
+import ogo.marcin.ox.io.Output;
 import ogo.marcin.ox.player.Player;
 import ogo.marcin.ox.player.PlayerAPI;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,28 +17,23 @@ public class Game {
     private final BoardAPI boardAPI;
     private final PlayerAPI playerAPI;
     private final Input input;
-    private final List<Match> matches;
+    private final Output output;
     private final List<Player> players;
 
-    private Game(Settings settings, BoardAPI boardAPI, PlayerAPI playerAPI, Input input,
-                 List<Match> matches, List<Player> players) {
+    public Game(Settings settings, BoardAPI boardAPI, PlayerAPI playerAPI,
+                 Input input, Output output, List<Player> players) {
         this.settings = settings;
         this.boardAPI = boardAPI;
         this.playerAPI = playerAPI;
         this.input = input;
-        this.matches = matches;
+        this.output = output;
         this.players = players;
-    }
-
-    public Game(Settings settings, BoardAPI boardAPI, PlayerAPI playerAPI, Input input, List<Player> players) {
-        this(settings, boardAPI, playerAPI, input, new LinkedList<>(), players);
     }
 
     public void play() {
         for (int i = 0; i < settings.getNumberOfRounds(); i++) {
             Judge judge = new Judge(boardAPI, settings);
             Match match = new Match(boardAPI, playerAPI, input, players, judge);
-            matches.add(match);
             match.play();
             boardAPI.clearBoard(settings.getDefaultSign());
             Collections.reverse(players);
