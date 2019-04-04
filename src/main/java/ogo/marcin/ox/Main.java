91bc6db016b664799550da42032d9d45b6c1878f
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-04-04 19:04:24 +0200

    Add Ouytput initialization and simplify settigns creation

--- src/main/java/ogo/marcin/ox/Main.java
+++ src/main/java/ogo/marcin/ox/Main.java
@@ -1,6 +1,8 @@
 package ogo.marcin.ox;
 
 import ogo.marcin.ox.board.*;
+import ogo.marcin.ox.io.Localization;
+import ogo.marcin.ox.io.Output;
 import ogo.marcin.ox.player.*;
 import ogo.marcin.ox.game.Game;
 import ogo.marcin.ox.game.Settings;
@@ -17,22 +19,25 @@ class Main {
         try(Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8)) {
             PlayerAPI playerAPI = new PlayerAPIImpl();
 
-            Input input = new Input(scanner);
+//            TODO problem with how set language without input
+            Output output = new Output(System.out, Localization.setLanguage("english"));
+            Input input = new Input(scanner, output);
+
             List<Player> players = playerAPI.createPlayers(input);
 
             Board board = new Board.BoardBuilder()
                     .withDimension(input.getBoardDimensions())
-                    .withDefaultSign()
                     .build();
             BoardAPI boardAPI = new BoardAPIImpl(board);
 
-            Settings settings = new Settings.SettingsBuilder(input)
-                    .withWinCondition(boardAPI.getBoardDimension())
-                    .withNumberOfRounds()
-                    .withDefaultSign()
+            int MIN_WIN_CONDITION = 3;
+            int MAX_WIN_CONDITION = boardAPI.getBoardDimension();
+
+            Settings settings = new Settings.SettingsBuilder()
+                    .withWinConditionInRange(input.getWinConditionInRange(MIN_WIN_CONDITION, MAX_WIN_CONDITION))
                     .build();
 
-            Game game = new Game(settings, boardAPI, playerAPI, input, players);
+            Game game = new Game(settings, boardAPI, playerAPI, input, output, players);
             game.play();
         }
     }
